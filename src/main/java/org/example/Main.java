3c35cfc3afa3d44de9f95a72e051b4c4b0e1adc2
İnci Gülçin Durak Yolcu <113159986+InciGulcinDY@users.noreply.github.com>
2023-10-24 10:07:29 +0300

    creating oop project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Product-1
+        Product product1 = new Product();
+        product1.setName("Delonghi Kahve Makinesi");
+        product1.setUnitPrice(7500);
+        product1.setDiscount(7);
+        product1.setUnitsInStock(3);
+        product1.setImageUrl("image.jpg");
+
+        //Product-2
+        Product product2 = new Product();
+        product2.setName("Smeg Kahve Makinesi");
+        product2.setUnitPrice(6500);
+        product2.setDiscount(8);
+        product2.setUnitsInStock(2);
+        product2.setImageUrl("image2.jpg");
+
+        //Product-3
+        Product product3 = new Product();
+        product3.setName("Kitchen Aid Kahve Makinesi");
+        product3.setUnitPrice(4500);
+        product3.setDiscount(9);
+        product3.setUnitsInStock(4);
+        product3.setImageUrl("image3.jpg");
+
+        Product[] products = {product1,product2,product3};
+
+        System.out.println("<ul>");
+        for (Product product : products) {
+            System.out.println("<li>" + product.getName() + "</li>");
+        }
+        System.out.println("</ul>");
+    }
+}
